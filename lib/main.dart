92f63df1d7b04a3607cdf5df0d61b9af91902469
Animesh <technochatter@yahoo.com>
2020-07-17 21:16:36 +0530

    Imported library and Class declared with main

---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart'; //importing material library for its beautiful reusable widgets
+
+void main() => runApp(MyApp()); //informing Flutter that MyApp() is our main class
+
+class MyApp extends StatelessWidget {
+
+}
