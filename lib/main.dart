8fe2578f9b1f9ea86868d24908dc01f980219dd3
Animesh <technochatter@yahoo.com>
2020-07-17 23:59:24 +0530

    Center Widget for centering all Nested Home page Content

--- lib/main.dart
+++ lib/main.dart
@@ -6,8 +6,10 @@ class MyApp extends StatelessWidget { //my custom class name is MyApp which is a
    @override //overriding the build method of StatelessWidget to modify
   Widget build(BuildContext context) { //Flutter calls this method to load the application content
      //return the app UI from this method
-     //create a container to hold all our home page contents
-     return Container();
+     return Container(//create a container to hold all our home page contents
+       //For nesting elements or containers need to declare them as Child
+       child: Center()//Center widget which aligns all its child content to middle of Screen
+     );
    }
 
 }
