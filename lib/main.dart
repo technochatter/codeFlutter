8d584b7cc30356c1432eb9d814828a2e316eecf2
Animesh <technochatter@yahoo.com>
2020-07-17 21:38:20 +0530

    Override and declare the build method

--- lib/main.dart
+++ lib/main.dart
@@ -3,5 +3,9 @@ import 'package:flutter/material.dart'; //importing material library for its bea
 void main() => runApp(MyApp()); //informing Flutter that MyApp() is our main class
 
 class MyApp extends StatelessWidget {
+   @override //overriding the build method of StatelessWidget to modify
+  Widget build(BuildContext context) { //Flutter calls this method to load the application content
+
+   }
 
 }
