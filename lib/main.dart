ba43f54362277dfe18ebc993aa2af1174857e3dd
Animesh <technochatter@yahoo.com>
2020-07-17 23:43:37 +0530

    create a container to hold all our home page contents

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,12 @@ import 'package:flutter/material.dart'; //importing material library for its bea
 
 void main() => runApp(MyApp()); //informing Flutter that MyApp() is our main class
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatelessWidget { //my custom class name is MyApp which is a StatelessWidget class
    @override //overriding the build method of StatelessWidget to modify
   Widget build(BuildContext context) { //Flutter calls this method to load the application content
-
+     //return the app UI from this method
+     //create a container to hold all our home page contents
+     return Container();
    }
 
 }
