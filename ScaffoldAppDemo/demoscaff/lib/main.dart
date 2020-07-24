96d709e6e19a8644b05b3254b77f57086f02ba85
Animesh <technochatter@yahoo.com>
2020-07-24 23:23:24 +0530

    Feeding the App UI to Main as a Scaffold returning from MyApp class

--- ScaffoldAppDemo/demoscaff/lib/main.dart
+++ ScaffoldAppDemo/demoscaff/lib/main.dart
@@ -2,8 +2,19 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(new Center(
-    child:new Text('Hello, world!',
-    textDirection: TextDirection.ltr,)
-  ));
+  runApp(new MaterialApp(
+    title: 'Hello World App',
+    home: new myApp(),
+  )
+  );
+}
+
+class myApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(title: Text('App Bar Title Text'),),
+      body: new Center(child: new Text('Body Text'),),
+    );
+  }
 }
